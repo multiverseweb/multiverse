ec4f62967e142c32ab2b4340a5276f724a7abb02
Tejas-Gupta-7 <115937671+Tejas-Gupta-7@users.noreply.github.com>
2022-12-19 04:39:09 +0530

    Update index.js

--- index.js
+++ index.js
@@ -17,3 +17,18 @@ function topFunction() {
   document.body.scrollTop = 0; // For Safari
   document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
 }
+
+$(document).ready(function() {
+  $("#toggle").click(function() {
+    var elem = $("#toggle").text();
+    if (elem == "Read More") {
+      //Stuff to do when btn is in the read more state
+      $("#toggle").text("Read Less");
+      $("#text").slideDown();
+    } else {
+      //Stuff to do when btn is in the read less state
+      $("#toggle").text("Read More");
+      $("#text").slideUp();
+    }
+  });
+});
