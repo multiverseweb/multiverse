6869c001425d19660df0a02d2cd68bf91d38ab7c
Tejas-Gupta-7 <115937671+Tejas-Gupta-7@users.noreply.github.com>
2022-12-24 05:54:57 +0530

    Update index.js

--- index.js
+++ index.js
@@ -17,3 +17,8 @@ function topFunction() {
   document.body.scrollTop = 0; // For Safari
   document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
 }
+
+function myFunction() {
+  document.getElementById("display").style.color = "black";
+document.getElementById("display").style.backgroumdColor = "white";
+}
