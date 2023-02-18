313ab421695ba29c3c9c283380e0eacf4d30ab16
Tejas-Gupta-7 <115937671+Tejas-Gupta-7@users.noreply.github.com>
2023-02-18 23:56:47 +0530

    Update index.js

--- index.js
+++ index.js
@@ -40,3 +40,18 @@ function myFunction() {
     document.getElementById("night").style.display = "none";
     document.getElementById("dark").style.display = "flex";
   }
+
+function myFunction() {
+  // Get the text field
+  var copyText = document.getElementById("myInput");
+
+  // Select the text field
+  copyText.select();
+  copyText.setSelectionRange(0, 99999); // For mobile devices
+
+  // Copy the text inside the text field
+  navigator.clipboard.writeText(copyText.value);
+  
+  // Alert the copied text
+  alert("Copied the link: " + copyText.value);
+}
