7b6ac29f31067a296c188876d2b44bcb881fef1e
Tejas Gupta <115937671+Tejas-Gupta-7@users.noreply.github.com>
2023-06-23 02:55:35 +0530

    Update index.js

--- index.js
+++ index.js
@@ -29,7 +29,7 @@ function topFunction() {
 function myFunction() {
 	document.getElementById("ss").style.filter="invert(1)";
 document.getElementById("building").style.filter ="invert(1)";
-	  document.getElementById("glass").style.background = "rgba(44, 44, 44, 0.145)";
+	  document.getElementById("glass").style.background = "rgba(244, 244, 244, 0.145)";
   document.getElementById("glass1").style.background = "rgba(244, 244, 244, 0.145)";
   document.getElementById("glass2").style.background = "rgba(244, 244, 244, 0.145)";
   document.getElementById("glass3").style.background = "rgba(244, 244, 244, 0.145)";
